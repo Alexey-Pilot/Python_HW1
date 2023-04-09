c497c096dbad9fa31339de1ad11f5ce1bb0252ea
Alexey-Pilot <alexragnarek@gmail.com>
2023-04-09 19:49:25 +0500

    finished

--- ex2.py
+++ ex2.py
@@ -1,12 +1,2 @@
-n = int(input("Insert the number: "))
-m = str(n)
-DigitsSum = 0
-while int(n) > 0:
-    DigitsSum += n % 10
-    n = n // 10
-print(f"Sum of digits equals to {DigitsSum}")
-
-print(f"Sum of digits equals to {sum([int(m[i]) for i in range(len(m))])}")
-
-DigitsSum2 = int(m[0]) + int(m[1]) + int(m[2])
-print(f"Sum of digits equals to {DigitsSum2}")
+n = input("Insert the number: ")
+print(f"Sum of digits equals to {sum([int(n[i]) for i in range(len(n))])}")
--- ex6.py
+++ ex6.py
@@ -1,5 +1,4 @@
 import random
-
 t = str(random.randint(100000, 1000000))
 print(f"Your ticket number is: {t}\n"
       f"{['Try again', 'WOW! Your ticket is Happy!!!'][(int(t[0]) + int(t[1])) + int(t[2]) == int(t[3]) + int(t[4]) + int(t[5])]}")
